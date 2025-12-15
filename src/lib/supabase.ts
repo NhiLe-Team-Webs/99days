0de56637bd25c61d3aa1bde616b52822d62a41f8
Nguyenvietduc795 <nguyenvietduc7925nlt@gmail.com>
2025-12-15 22:30:02 +0700

    update UI

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = "https://dcbyllsbsassmgirygfi.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjYnlsbHNic2Fzc21naXJ5Z2ZpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU5OTAxMDgsImV4cCI6MjA3MTU2NjEwOH0.n449JZesGj978cs3tEaxwwN5j_nae8U9gsJSoE6w0Oo";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
